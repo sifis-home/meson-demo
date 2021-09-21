513994cf15ba9111da5aadd8e1ba0ccf32c1ff53
Luni-4 <luni-4@hotmail.it>
2021-09-21 12:46:13 +0200

    Remove goto
    In this way the code is much simpler to follow through

--- lib/foo.c
+++ lib/foo.c
@@ -14,21 +14,20 @@ Foo *foo_new(void) {
     // Use calloc to ensure the fields are 0-initialized so
     // you can pass it to foo_free even if partially allocated.
     Foo *foo = calloc(1, sizeof(*foo));
-    if (!foo)
-        goto fail;
+    if (!foo) {
+        foo_free(&foo);
+        return NULL;
+    }
 
     foo->buf = calloc(N_ELEMENTS, sizeof(*foo->buf));
-    if (!foo->buf)
-        goto fail;
+    if (!foo->buf) {
+        foo_free(&foo);
+        return NULL;
+    }
 
     foo->len = N_ELEMENTS;
 
     return foo;
-
-fail:
-    foo_free(&foo);
-
-    return NULL;
 }
 
 void foo_free(Foo **foo) {
