a35f7c4e6be9fc239ed138ab281e2f5ad2ac3170
Luni-4 <luni-4@hotmail.it>
2021-10-13 14:47:48 +0200

    Fix invalid conversions

--- lib/foo.c
+++ lib/foo.c
@@ -13,13 +13,13 @@ struct Foo {
 Foo *foo_new(void) {
     // Use calloc to ensure the fields are 0-initialized so
     // you can pass it to foo_free even if partially allocated.
-    Foo *foo = calloc(1, sizeof(*foo));
+    Foo *foo = (Foo *) calloc(1, sizeof(*foo));
     if (!foo) {
         foo_free(&foo);
         return NULL;
     }
 
-    foo->buf = calloc(N_ELEMENTS, sizeof(*foo->buf));
+    foo->buf = (int *) calloc(N_ELEMENTS, sizeof(*foo->buf));
     if (!foo->buf) {
         foo_free(&foo);
         return NULL;
